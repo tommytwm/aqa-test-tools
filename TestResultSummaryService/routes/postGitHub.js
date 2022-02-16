035536661837cde96c0d40b9d4e7adf138ece430
Tommy Tho <tommythowm@gmail.com>
2022-02-15 21:53:05 -0800

    fixed network call

--- TestResultSummaryService/routes/postGitHub.js
+++ TestResultSummaryService/routes/postGitHub.js
@@ -5,28 +5,31 @@ const FormData = require('form-data');
 
 module.exports = async (req, res) => {
     const doFetch = async (...args) => {
-        return (await fetch(...args)).json();
+        return (await fetch(...args));
     };
 
-    const file = fs.readFileSync("./README.md");
+    const file = fs.readFileSync("../<file_in_root>");
     const form1 = new FormData();
-    form1.append("content_type", "image/jpeg");
-    form1.append("authenticity_token", "__");
+    form1.append("content_type", "multipart/form-data");
+    form1.append("authenticity_token", "<token>");
+    form1.append("repository_id", "<id>");
     form1.append("size", file.length);
-    form1.append("name", "README.md");
+    form1.append("name", "<file_name>");
 
     const { upload_url, asset, form } = await doFetch(
         "https://github.com/upload/policies/assets", {
             headers: {
                 accept: "application/json",
                 ...form1.getHeaders(),
-                cookie: "user_session=<cookie>; __Host-user_session_same_site=<cookie>;"
+                cookie: "<cookie>"
             },
             body: form1.getBuffer(),
             method: "POST",
+            'User-Agent': '*'
         }
     );
+
     console.log(upload_url, asset, form);
 
-    res.send( asset, form );
+    // res.send( asset, form );
 };
