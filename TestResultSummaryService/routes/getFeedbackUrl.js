391fee3d155369f6f04881b99e7795ced01640fc
Tommy Tho <tommythowm@gmail.com>
2022-03-04 14:36:12 -0800

    added TODO

--- TestResultSummaryService/routes/getFeedbackUrl.js
+++ TestResultSummaryService/routes/getFeedbackUrl.js
@@ -5,6 +5,7 @@ module.exports = async (req, res) => {
     if (!repoName || !buildName || !issueName || !issueCreator || !accuracy) {
         res.send({ error: 'Input parameters are missing' });
     } else {
+        // TODO: Get Feedback info from db
         res.send({ output: { result: 'success' } });
     }
 };
