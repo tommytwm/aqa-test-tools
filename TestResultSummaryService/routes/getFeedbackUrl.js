405e0adb0fd4d412c9baa6036ca39ab5bae2aefc
Tommy Tho <tommythowm@gmail.com>
2022-03-07 23:00:38 -0800

    added get endpoint in backend

--- TestResultSummaryService/routes/getFeedbackUrl.js
+++ TestResultSummaryService/routes/getFeedbackUrl.js
@@ -7,10 +7,11 @@ module.exports = async (req, res) => {
     if (!repoName || !buildName || !issueName || !issueCreator || !accuracy) {
         res.send({ error: 'Input parameters are missing' });
     } else {
-        // TODO: Get Feedback info from db
         const db = new UserFeedbackDB();
-        const result = await db.getData({issueNumber: "1234", issueCreator: "tommy-test"}).toArray();
-        console.log(result);
-        res.send({ output: { result: 'success' } });
+        const result = await db.getData(issueNumber, issueName).toArray();
+        if (!result || result.length === 0 ) {
+            res.send({ output: null }); 
+        }
+        res.send({ output: { result } });
     }
 };
